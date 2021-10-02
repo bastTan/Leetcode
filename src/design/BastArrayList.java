5acef85a969abb33ac2a1a8da8ba293b9f778a1f
Bast <bast.p.tan@gamil.com>
2021-10-02 00:00:37 -0700

    Override toString() to replace print(), fix corner cases in insert.

--- src/design/BastArrayList.java
+++ src/design/BastArrayList.java
@@ -7,14 +7,14 @@ import java.util.Arrays;
 a simple ArrayList implementation.
 when array size > capacity, capacity * 2, copy original array to the new array.
  */
-public class MyArrayList {
+public class BastArrayList {
     private static final int DEFAULT_CAPACITY = 1;
 
     private int[] vals;
     private int capacity = DEFAULT_CAPACITY;
     private int length = 0;
 
-    public MyArrayList() {
+    public BastArrayList() {
         this.vals = new int[this.capacity];
     }
     public int getLength() {
@@ -35,19 +35,23 @@ public class MyArrayList {
         this.vals[index] = val;
     }
     // set(index, val), then shift the rest
-    // insert do not allow appened
+    // if insert index == length, append
     public void insert(int index, int val) {
+        // insert at index == append at the end
+        if (index == this.length) {
+            add(val);
+            return;
+        }
         checkIndex(index);
         this.length++;
         ensureCapacity();
-        int prevVal = this.vals[index];
-        int currVal = 0;
-        for (int i = index+1; i < this.length; i++) {
-            currVal = prevVal;
-            prevVal = this.vals[i];
+
+        int currVal = val;
+        for (int i = index; i < this.length; i++) {
+            int tmp = this.vals[i];
             this.vals[i] = currVal;
+            currVal = tmp;
         }
-        this.vals[index] = val;
     }
     // delete arr[i], move the rest to the left
     public void delete(int index) {
@@ -60,30 +64,29 @@ public class MyArrayList {
         this.length--;
     }
 
-    public void print() {
+    @Override
+    public String toString() {
         System.out.println("===========================================");
         System.out.println("current size = " + this.length + ", " +
                         "current capacity = " + this.capacity);
-        if (length == 0) {
-            System.out.println("{}");
-            return;
-        }
 
-        System.out.print('{');
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
         for (int i = 0; i < this.length; i++) {
-            System.out.print(vals[i]);
+            sb.append(this.vals[i]);
             if (i < this.length - 1) {
-                System.out.print(", ");
+                sb.append(", ");
             }
         }
-        System.out.println('}');
+        sb.append("]");
+        return sb.toString();
     }
 
     private void ensureCapacity(){
         if (length > capacity) {
             int[] newVals = Arrays.copyOf(this.vals, capacity * 2 );
             this.vals = newVals;
-            this.capacity = this.capacity * 2;
+            this.capacity *= 2;
         }
     }
     private void checkIndex(int index) {
@@ -93,47 +96,51 @@ public class MyArrayList {
     }
 
     public static void main(String[] args) {
-        MyArrayList mylist = new MyArrayList();
-        mylist.print();
+        BastArrayList mylist = new BastArrayList();
+        System.out.println(mylist);
         mylist.add(1);
         mylist.add(3);
-        mylist.print();
+        System.out.println(mylist);
         try {
             System.out.println(mylist.get(1));
             mylist.set(1, 4);
-            mylist.print();
+            System.out.println(mylist);
         }
         catch (Exception e) {
             System.out.println(e);
         }
         mylist.add(3);
-        mylist.print();
+        System.out.println(mylist);
         mylist.add(5);
+        System.out.println(mylist);
+        mylist.delete(3);
+        System.out.println(mylist);
         mylist.add(6);
         mylist.add(9);
-        mylist.print();
+        System.out.println(mylist);
         mylist.insert(2, 2);
-        mylist.print();
+        System.out.println(mylist);
         mylist.insert(0, 2);
-        mylist.print();
+        System.out.println(mylist);
         mylist.insert(4, 8);
-        mylist.print();
+        System.out.println(mylist);
         mylist.insert(8, 10);
-        mylist.print();
+        System.out.println(mylist);
         mylist.insert(9, 10);
-        mylist.print();
+        System.out.println(mylist);
         mylist.add(11);
-        mylist.print();
+        System.out.println(mylist);
         mylist.delete(1);
-        mylist.print();
+        System.out.println(mylist);
         while (mylist.getLength() > 1) {
             mylist.delete(0);
         }
-        mylist.print();
+        System.out.println(mylist);
         mylist.delete(0);
-        mylist.print();
+        System.out.println(mylist);
         mylist.add(1);
-        mylist.print();
+        System.out.println(mylist);
+        System.out.println(mylist);
     }
 }
 
